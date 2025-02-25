e06a351b341963af8e6dfd1e85a87f8c4a096c25
emili <emili@DESKTOP-T01GNG5>
2025-02-25 20:55:27 +0200

    Add project files.

---
+++ 25.02.2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _25._02._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
